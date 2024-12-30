f9e9aef16024e605708eed53ec66247bd8446f8d
Hashir Shahzad <hashir447079@gmail.com>
2024-12-30 16:27:13 +0500

    test

--- player/player.gd
+++ player/player.gd
@@ -4,26 +4,19 @@ class_name Players
 @export_group("Physics")
 @export var speed : int = 300
 @export var friction : float = 0.08
-@export var acceleration : float = 0.04
+@export var acceleration : float = 0.4
 
+var direction : Vector2
 var wish_dir : Vector2
 
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if direction:
-		velocity.x = direction.x * speed
-		velocity.y = direction.y * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.y = move_toward(velocity.y, 0, speed)
-
+	move()
 	move_and_slide()
 
 func move() -> void:
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	wish_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	direction = (Vector2(wish_dir.x, wish_dir.y)).normalized()
-	
 	if direction:
 		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
 	else:
