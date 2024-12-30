9da7979adce786cd8ed91f3f856605dc7bc01c99
Hashir Shahzad <hashir447079@gmail.com>
2024-12-30 16:19:42 +0500

    Added Morie Stuff

--- player/player.gd
+++ player/player.gd
@@ -1,19 +1,31 @@
 extends CharacterBody2D
 class_name Players
 
-const SPEED = 300.0
+@export_group("Physics")
+@export var speed : int = 300
+@export var friction : float = 0.08
+@export var acceleration : float = 0.04
+
+var wish_dir : Vector2
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction.x * speed
+		velocity.y = direction.y * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
 
 	move_and_slide()
+
+func move() -> void:
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	direction = (Vector2(wish_dir.x, wish_dir.y)).normalized()
+	
+	if direction:
+		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction)
+	
