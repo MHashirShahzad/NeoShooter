67dff258887c2af5dabe7c68f372d36029ca38bf
Abdullah-Zeeshan-github <azqadar123@gmail.com>
2025-01-02 16:43:12 +0500

    ui bg removed

--- autoloads/ui/ui_manager.gd
+++ autoloads/ui/ui_manager.gd
@@ -8,6 +8,8 @@ func _process(delta: float) -> void:
 
 
 func update_health():
+	if not (GameManager.p1 or GameManager.p2):
+		return
 	var p1_health : float =  GameManager.p1.health
 	var p2_health : float = GameManager.p2.health
 	label.text = "P1: " + str(p1_health) + " P2: " + str(p2_health)
