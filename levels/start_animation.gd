32e0bfe720ba24cef633ec38d8c29120c60e75d9
Hashir Shahzad <hashir447079@gmail.com>
2025-01-26 15:23:34 +0500

    Some stupidity

--- levels/start_animation.gd
+++ levels/start_animation.gd
@@ -13,16 +13,16 @@ enum STATES {
 	NONE
 }
 
-var current_state : STATES 
+var current_state : STATES
 
 func _input(event: InputEvent) -> void:
 	if !event.is_pressed:
 		return
-	
+
 	# make it accept keyboard controller and mouse keys
 	if ! (event is InputEventKey or event is InputEventJoypadButton or event is InputEventMouseButton):
 		return
-		
+
 	match current_state:
 		STATES.HOMIE:
 			homie_despawn_ani()
@@ -39,14 +39,14 @@ func _ready() -> void:
 	parse_command_line_args()
 
 	# await ani_player.animation_finished
-	
+
 func homie_spawn_ani():
 	current_state = STATES.HOMIE
 	var tween = get_tree().create_tween()
 	tween.tween_property(homie_label, "self_modulate", Color.WHITE, .5)
 	await  tween.finished
 	tween.kill()
-	
+
 func homie_despawn_ani():
 	var tween = get_tree().create_tween()
 	tween.tween_property(homie_label, "self_modulate", Color.TRANSPARENT, .5)
@@ -60,7 +60,7 @@ func yellow_spawn_ani():
 	tween.tween_property(yellow_label, "self_modulate", Color.WHITE, .5)
 	await  tween.finished
 	tween.kill()
-	
+
 func yellow_despawn_ani():
 	current_state = STATES.NONE
 	var tween = get_tree().create_tween()
@@ -71,7 +71,7 @@ func yellow_despawn_ani():
 
 func parse_command_line_args():
 	var arguments : Dictionary = {}
-	
+
 	for argument in OS.get_cmdline_args():
 		if argument.contains("="):
 			var key_value = argument.split("=")
@@ -80,28 +80,36 @@ func parse_command_line_args():
 			# Options without an argument will be present in the dictionary,
 			# with the value set to an empty string.
 			arguments[argument.trim_prefix("--")] = ""
-	
+
 	## PURE BRAIN ROT COLORS
 	print_rich("[color=red]C[/color][color=blue]M[/color][color=green]D[/color] [color=yellow]L[/color][color=cyan]I[/color][color=magenta]N[/color][color=orange]E[/color] ARGS EXIST!!!!")
 	print_rich("[color=red] SYNTAX: --val1 --val2 [/color]")
 
 	print("==============================================")
-	
+
 	# EN
 	if arguments.keys().has("en") or arguments.keys().has("EN"):
 		print_rich("[color=green] https://discord.gg/eliteninjas [/color]")
-		
+
 	if arguments.keys().has("clara"):
 		print("meow ?")
 		Input.set_custom_mouse_cursor(load("res://assets/clara-cursor.png"))
 
 	if arguments.keys().has("royn"):
 		print("SKILL ISSUE")
-	
+
+## add rich here @HASHIR
+	if arguments.keys().has("loe"):
+		print("is earth lost or are we lost on earth")
+
+	if arguments.keys().has("pentagram"):
+			print(":skull:💀")
+
+
 	if arguments.keys().has("yellow"):
 		print("Peela Admi fr")
-	
+
 	if arguments.keys().has("1983"):
 		print("EVAN WAS INNOCENT!")
-	
+
 	print("==============================================")
